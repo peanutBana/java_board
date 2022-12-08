1ec368b165cf0a5f740d7cb7330f9417c471772f
peanutBana <diao0321@gmail.com>
2022-12-08 23:49:27 +0900

    create_project

---
+++ src/application/BoardApplication.java
@@ -0,0 +1,20 @@
+package application;
+
+public class BoardApplication {
+	
+	/*
+	    * ArrayList와 HashMap을 사용해 게시판 테이블을 만들고,
+	    * 조회, 등록, 수정, 삭제가 가능한 게시판을 만들어주세요.
+	    * 목록출력, 게시글 내용 볼 수 있음
+	    * 게시물의 데이터를 저장 
+	    * 번호, 제목, 내용, 작성자, 작성일자 - key로 사용
+	    * 목록에서는 조회, 등록, 종료
+	    * 상세화면에서는 수정, 삭제, 목록
+	 */
+	
+	
+		
+	public static void main(String[] args) {
+		System.out.println("\t자유 게시판");
+	}
+}
